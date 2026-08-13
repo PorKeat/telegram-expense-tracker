c2b27ae38b7d05014303d348658085b434987204
PorKeat <alexkgm2412@gmail.com>
2026-08-13 10:36:04 +0700

    fix: suppress hydration warnings on html and body tags for telegram sdk

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -7,12 +7,12 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
         <script src="https://telegram.org/js/telegram-web-app.js" async></script>
       </head>
-      <body>
+      <body suppressHydrationWarning>
         {children}
       </body>
     </html>
